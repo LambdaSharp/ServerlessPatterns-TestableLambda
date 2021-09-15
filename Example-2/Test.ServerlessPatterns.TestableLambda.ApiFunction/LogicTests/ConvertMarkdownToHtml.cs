ca17c64f5846ff5cff2708c1ec180de0cc4df1ab
Steve Bjorg <steve.bjorg@gmail.com>
2021-09-15 14:46:21 -0700

    added test project

---
+++ Example-2/Test.ServerlessPatterns.TestableLambda.ApiFunction/LogicTests/ConvertMarkdownToHtml.cs
@@ -0,0 +1,69 @@
+using System;
+using Xunit;
+
+namespace Test.ServerlessPatterns.TestableLambda.ApiFunction.LogicTests {
+
+    public class ConvertMarkdownToHtml {
+
+        //--- Methods ---
+
+        [Fact]
+        public void Test1( ) {
+
+            // arrange
+
+            // act
+
+            // assert
+
+        }
+    }
+
+    public class CreatePostAsync {
+
+        //--- Methods ---
+
+        [Fact]
+        public void Test1( ) {
+
+            // arrange
+
+            // act
+
+            // assert
+
+        }
+    }
+
+    public class ViewPostAsync {
+
+        //--- Methods ---
+
+        [Fact]
+        public void Test1( ) {
+
+            // arrange
+
+            // act
+
+            // assert
+
+        }
+    }
+
+    public class ListPostsAsync {
+
+        //--- Methods ---
+
+        [Fact]
+        public void Test1( ) {
+
+            // arrange
+
+            // act
+
+            // assert
+
+        }
+    }
+}
